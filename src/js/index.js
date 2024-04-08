72ee74b068a7461df30233fd80048f17e9fc79e1
Héric Gomes da Silva <h1g2s0@hotmail.com>
2024-04-08 09:45:37 -0300

    JS

--- src/js/index.js
+++ src/js/index.js
@@ -13,14 +13,31 @@ OBJEITVO 1 - quando clicarmos na seta avançar temos que mostrar o próximo cart
 */ 
 
 const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar')
 const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
 
-const cartaoSelecionado = document.querySelector('.selecionado');
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add('selecionado');
+}
 
 btnAvancar.addEventListener('click', function(){
-    cartaoAtual++;
-    cartoes[cartaoAtual].classList.add('selecionado');
+    if (cartaoAtual === cartoes.length - 1) return; 
 
-    cartaoSelecionado.classList.remove('selecionado');
+    esconderCartaoSelecionado();
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
 })
+
+btnVoltar.addEventListener('click', function(){
+    if(cartaoAtual === 0) return;
+    
+    esconderCartaoSelecionado();
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
